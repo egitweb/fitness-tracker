403a3e861d6ebc7bc37254738f6b1476791b5906
Eugenio Espinoza <68721930+egitweb@users.noreply.github.com>
2021-02-05 22:10:54 -0600

    routes: APIs added

---
+++ routes/api.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const Workout = require("../models").Workout;
+
+
+//ROUTER to GET Workouts
+router.get("/api/workouts", (req, res) => {
+  
+});
+
+//ROUTER to PUT Workouts
+router.put("/api/workouts/:id", (req, res) => {
+  
+});
+
+//ROUTER to Post Workouts
+router.post("/api/workouts", (req, res) => {
+  
+});
+
+//ROUTER to GET Workout range
+router.get("/api/workouts/range", (req, res) => {
+
+});
+
+//ROUTER to DELETE Workouts
+router.delete("/api/workouts", (req, res) => {
+ 
+});
+
+module.exports = router;
